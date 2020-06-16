bf217879dc6178fa1f014546937e34a950077bec
Silva404 <brenosilva0252@gmail.com>
2020-06-16 18:24:52 -0300

    finalmente acabeeei

--- scripts/recipes.js
+++ scripts/recipes.js
@@ -2,24 +2,11 @@ const cards = document.querySelectorAll('.card')
 const modal = document.querySelector('#modal')
 const receitaShow = document.querySelector('#modal .receita')
 
-//a imagem especifica de cada card
-const cardImg = document.querySelectorAll('.card img')
-
-// for (let img of cardImg) {    
-//     img.addEventListener('click', () => {
-//         modal.classList.remove('hide')
-//         receitaShow.innerHTML += `<img src='${img.src}'></img>`
-//     })
-// }
-
-
 for (let card of cards) {    
     card.addEventListener('click', () => {
-        console.log(card.getElementsByClassName('.teste').innerHTML)
 
         modal.classList.remove('hide')
-        receitaShow.innerHTML += `<h4>${card.innerHTML}</h4> 
-        <p>${card.getElementsByClassName('.teste').innerHTML}</p>`
+        receitaShow.innerHTML += `${card.innerHTML}`
     })
 }
 
