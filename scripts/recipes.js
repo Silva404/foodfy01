63d378f316b4b6ef4adeef6cd3595e18733a909f
Silva404 <brenosilva0252@gmail.com>
2020-06-16 18:15:32 -0300

    falta so concluir o modal

--- scripts/recipes.js
+++ scripts/recipes.js
@@ -1,15 +1,45 @@
 const cards = document.querySelectorAll('.card')
 const modal = document.querySelector('#modal')
+const receitaShow = document.querySelector('#modal .receita')
 
-for (let card of cards) {
-    card.addEventListener('click', () => {
+//a imagem especifica de cada card
+const cardImg = document.querySelectorAll('.card img')
+
+for (let img of cardImg) {    
+    img.addEventListener('click', () => {
         modal.classList.remove('hide')
-
+        receitaShow.innerHTML += `<img src='${img.src}'></img>`
     })
 }
+
+
+for (let card of cards) {    
+    card.addEventListener('click', () => {
+        console.log(card.textContent)
+        console.log(card.innerHTML)
+
+        modal.classList.remove('hide')
+        receitaShow.innerHTML += `<h4>${card.textContent}</h4> 
+        <p>${card.getAttribute('p')}</p>`
+    })
+}
+
+
+
+// quando eu clicar no card of cards
+
+
+// tenho que pegar sua image
+
+// tittle h4
+// pegar o text do event.target e colocar no meu h4 e p inner html
+// subtitle p
+
+// apagar o conteudo quando clicar em fechar o modal
 
 document
     .querySelector('#close')
     .addEventListener('click', () => {
         modal.classList.add('hide')
+        receitaShow.innerHTML = ''
     })
