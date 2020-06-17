38cb0228e4ad03362ab87203c4a5b5d9b8a62541
Silva404 <brenosilva0252@gmail.com>
2020-06-17 01:09:38 -0300

    finalmente acabeeei

--- scripts/recipes.js
+++ scripts/recipes.js
@@ -10,19 +10,6 @@ for (let card of cards) {
     })
 }
 
-
-
-// quando eu clicar no card of cards
-
-
-// tenho que pegar sua image
-
-// tittle h4
-// pegar o text do event.target e colocar no meu h4 e p inner html
-// subtitle p
-
-// apagar o conteudo quando clicar em fechar o modal
-
 document
     .querySelector('#close')
     .addEventListener('click', () => {
