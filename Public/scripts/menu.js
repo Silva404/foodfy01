684531915ea7374709f30121fb467364d9a4d064
Silva404 <brenosilva0252@gmail.com>
2020-06-21 18:37:02 -0300

    hamburger finalizado

--- Public/scripts/menu.js
+++ Public/scripts/menu.js
@@ -1,1 +1,17 @@
-const menu = document.querySelector('.')
+let show = true
+
+const menu = document.querySelector('.menu-section')
+const header = document.querySelector('header')
+const content = document.querySelector('.content')
+
+
+menu.addEventListener('click', e => {
+    document.body.style.overflow = show ? 'hidden' : 'initial'
+
+    menu.classList.toggle('on', show)
+    show = !show  
+
+
+    header.classList.toggle('on')     
+    content.classList.toggle('on')
+})
